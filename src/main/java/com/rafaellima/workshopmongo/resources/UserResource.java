3f5d73a95f36558b2941667f5d4c48424841bfa5
Rafael Lima <rafael_lbs@hotmail.com>
2020-07-28 14:00:52 -0300

    Obtendo um usuario por id

--- src/main/java/com/rafaellima/workshopmongo/resources/UserResource.java
+++ src/main/java/com/rafaellima/workshopmongo/resources/UserResource.java
@@ -1,13 +1,14 @@
 package com.rafaellima.workshopmongo.resources;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rafaellima.workshopmongo.domain.User;
@@ -26,7 +27,13 @@ public class UserResource {
 		List<User> list = service.findAll();
 		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDto);
-		
 	}
 
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<UserDTO> findById(@PathVariable String id){
+		User obj = service.findById(id);
+		
+		
+		return ResponseEntity.ok().body(new UserDTO(obj));
+	}
 }
---
+++ src/main/java/com/rafaellima/workshopmongo/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,12 @@
+package com.rafaellima.workshopmongo.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+	
+	public ObjectNotFoundException(String msg) {
+		super();
+	}
+	
+
+}
