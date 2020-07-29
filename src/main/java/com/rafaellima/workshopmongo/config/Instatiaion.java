b369cd951ff9edb1f1b58f69691c685c238b289d
Rafael Lima <rafael_lbs@hotmail.com>
2020-07-29 11:09:33 -0300

    Endpoint para retornar os posts de um usuario

--- src/main/java/com/rafaellima/workshopmongo/config/Instatiaion.java
+++ src/main/java/com/rafaellima/workshopmongo/config/Instatiaion.java
@@ -43,7 +43,7 @@ public class Instatiaion implements CommandLineRunner {
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
-		maria.getPost().addAll(Arrays.asList(post1, post2));
+		maria.getPosts().addAll(Arrays.asList(post1, post2));
 		userRepository.save(maria);
 	}
 	
